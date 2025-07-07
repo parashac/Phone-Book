796b9abe2d75323ab58c2bf1a1b55b4348431521
parashac <parashacharya139@gmail.com>
2025-07-07 22:45:22 +0545

    Add HTML form structure with input fields for name, phone, address, gender, country, and courses

---
+++ submit.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$address = $_POST['address'];
+$phone = $_POST['phone'];
+
+$gender = isset($_POST['gender']) ? $_POST['gender'] : null;
+
+$country = isset($_POST['country']) ? $_POST['country'] : null;
+
+$courses = isset($_POST['courses']) ? implode(", ", $_POST['courses']) : null;
+
+$con = mysqli_connect("localhost", "root", "", "form");
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "INSERT INTO users (name, address, phone, gender, country, courses)
+          VALUES ('$name', '$address', '$phone', '$gender', '$country', '$courses')";
+
+if (mysqli_query($con, $query)) {
+    echo "Data inserted successfully!";
+} else {
+    echo "Error: " . mysqli_error($con);
+}
+
+mysqli_close($con);
+?>
